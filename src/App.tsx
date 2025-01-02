d3b48a00768d5ced7ee0ba0c97db4d1dff34ec69
Mark Paul <markpaulbusiness66@gmail.com>
2025-01-02 22:06:57 +0300

    Done with the header

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+ 
+  return (
+    <div className=" bg-slate-200 h-lvh">
+      <h1 className=" text-4xl font-bold flex justify-center text-blue-950 pl-11 pt-6">Welcome to your P.A.M</h1>
+    </div>
+  )
+}
+
+export default App
