9c8f467dfea747c08d3f8df5315b57e58ef0c638
Mark Paul <markpaulbusiness66@gmail.com>
2025-01-03 14:54:22 +0300

    Prepared a nice lil patients table

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
   if(isSuccess){
 
     return (
-      <div className=" bg-slate-200 h-lvh">
+      <div className=" bg-blue-50 h-lvh">
         <h1 className=" text-4xl font-bold flex justify-center text-blue-950 pl-11 pt-6">Welcome to your P.A.M</h1>
       
         <AppointmentList data={data}/>
--- src/AppointmentList.tsx
+++ src/AppointmentList.tsx
@@ -4,13 +4,16 @@ export default function AppointmentList ({data}){
 
     return (
         <>
-            <div>
-                <table className = " table-auto">
-                    <thead>
+            <div className=" flex justify-center items-center">
+                <table className = " table-auto border-2 rounded-lg mt-16">
+                    <thead className=" bg-blue-100">
                         <tr className=" text-gray-500 ">
-                            <th className=" font-normal">Patient ID</th>
-                            <th className=" font-normal">Patient Name</th>
-                            <th className=" font-normal">Date Check-in</th>
+                            <th className=" font-semibold  p-5"><input type="checkbox" name="" id="" /></th>
+                            <th className=" font-semibold  p-5">Patient ID</th>
+                            <th className=" font-semibold  p-5">Patient Name</th>
+                            <th className=" font-semibold  p-5">Date Check-in</th>
+                            <th className=" font-semibold  p-5">Time</th>
+                            <th className=" font-semibold  p-5">Status</th>
                         </tr>
                     </thead>
 
@@ -18,12 +21,12 @@ export default function AppointmentList ({data}){
                         {
                             data.map(item=>(
                             <tr key={item.id}>
-                                <td><input type="checkbox" name="" id="" /></td>
-                                <td>{item.id}</td>
-                                <td>{item.patientName}</td>
-                                <td>{item.date}</td>
-                                <td>{item.time}</td>
-                                <td>{item.status}</td>
+                                <td className=" px-5 py-2"><input type="checkbox" name="" id="" /></td>
+                                <td className=" px-5 py-2">{item.id}</td>
+                                <td className=" px-5 py-2">{item.patientName}</td>
+                                <td className=" px-5 py-2">{item.date}</td>
+                                <td className=" px-5 py-2">{item.time}</td>
+                                <td className=" px-5 py-2">{item.status}</td>
                             </tr>
                             ))
                         }
