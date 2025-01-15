384391d2a9ed4a666a99d384b0b235e995030852
Mark Paul <markpaulbusiness66@gmail.com>
2025-01-15 12:56:58 +0300

    Added a appontments list

---
+++ src/features/appontments/components/AppointmentList.tsx
@@ -0,0 +1,29 @@
+import { useGetAppointmentsQuery, useDeleteAppointmentMutation} from "../Appointmentslice";
+
+const AppointmentList = () => {
+    const {data: appointments, isLoading, error} = useGetAppointmentsQuery()
+    const [deleteAppointment] = useDeleteAppointmentMutation()
+
+    if(isLoading) return <p>Loading...</p>
+    if(error) return <p>Error loading appointments!</p>
+
+    return (
+        <div>
+            <h2>Appointments</h2>
+            <ul>
+                {
+                    appointments?.map((appointment)=>(
+                        <li key={appointment.id}>
+                            <p>
+                                {appointment.patientName} - {appointment.date} at {appointment.time}
+                            </p>
+                            <button onClick={()=> deleteAppointment(appointment.id)}>Delete</button>
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+    )
+}
+
+export default AppointmentList
